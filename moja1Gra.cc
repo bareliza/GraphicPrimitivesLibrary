7d76dd5ac35b3825a751b14b77b7f920229586d7
Jaroslaw Slebocki <j.k.slebocki@gmail.com>
2020-12-16 14:02:17 +0100

    gra przymiarki cd.

--- moja1Gra.cc
+++ moja1Gra.cc
@@ -7,27 +7,37 @@ Klawiatura K;
 int ostatniX, ostatniY;
 int ostatniKolor;
 
-void linia(int x0,int y0, int x1, int y1, unsigned int kolor)
-{
+void linia(int x0,int y0, int x1, int y1, unsigned int kolor) {
   P.linia(x0,y0,x1,y1,kolor);
   ostatniX = x1;
   ostatniY = y1;
   ostatniKolor = kolor;
 }
-
-void liniaDo(int x, int y, unsigned int kolor)
-{
+void linia(punkt a, punkt b, unsigned int kolor) {
+	linia(a.x, a.y, b.x, b.y, kolor);
+}
+// do pomyslenia, jak to sie powinno scalic
+void liniaOPCJE(punkt a, punkt b, unsigned int kolor, int dluzej) {
+	P.linia(a.x, a.y, b.x, b.y, kolor, dluzej);
+}
+void liniaOPCJE(punkt a, punkt b, unsigned int kolor, int dluzej, int kropki) {
+	P.linia(a.x, a.y, b.x, b.y, kolor, dluzej, kropki);
+}
+void liniaDo(int x, int y, unsigned int kolor) {
   P.linia(ostatniX, ostatniY, x, y, ostatniKolor);
   ostatniX = x;
   ostatniY = y;
   ostatniKolor = kolor;
 }
-
-void liniaDo(int x, int y)
-{
-  liniaDo(x,y,ostatniKolor);
+void liniaDo(punkt p, unsigned int kolor) {
+	liniaDo(p.x, p.y, kolor);
 }
-
+void liniaDo(int x, int y) {
+  liniaDo(x, y, ostatniKolor);
+}
+void liniaDo(punkt p) {
+  liniaDo(p.x, p.y);
+}
 
 void wypelnionaElipsa(int x, int y, int rx, int ry, double a0, double a1, double a2, unsigned int kolor) {
 	int pen1;
@@ -37,6 +47,20 @@ void wypelnionaElipsa(int x, int y, int rx, int ry, double a0, double a1, double
 		P.elipsa3(x,y,rx-4,i,a0,a1,a2,kolor);	
 	}
 	P.pisak(pen1);
+}
+
+void wypelnionaElipsa(punkt p, int rx, int ry, double a0, double a1, double a2, unsigned int kolor) {
+	wypelnionaElipsa(p.x, p.y, rx, ry, a0, a1, a2, kolor);
+}
+
+void wypelnionyOkrag(punkt p, int r, unsigned int kolor) {
+	// kolor <-> r
+	P.rysujPunkt4(p.x, p.y, kolor, r);
+}
+
+void wypelnionyOkrag(int x, int y, int r, unsigned int kolor) {
+	// kolor <-> r
+	P.rysujPunkt4(x, y, kolor, r);
 }
 
 int main(int argc, char **argv){
@@ -60,68 +84,113 @@ int main(int argc, char **argv){
   for(int j=0;j<P.wymiar.x;j+=100)
 	P.linia(j,0,j,P.wymiar.x,0xa00000);
 
-  P.rysujPunkt4(60+90-2,260+105+1-3,0xc04000,27);
-//  P.elipsa3(150-2,365-3,29,90,0,180,170,0x602000);	  
-  wypelnionaElipsa(150-2,365-3,30-3,92+3,0,180,169,0xa02000);	  
+  punkt punkty[30];
+
   int Xkolano1, Ykolano1;
   Xkolano1 = 150-2;
   Ykolano1 = 365-3;
+  punkt& kolano1 = punkty[0];
+  kolano1 = punkt(150-2,365-3+1);  
+  punkt& udo1 = punkty[1];
+  udo1 = kolano1 + punkt(0,-1);
+  punkt& lydka1 = punkty[2];
+  lydka1 = kolano1 + punkt(-41+2,52-1-1);
+
+  // kolano1
+  wypelnionyOkrag(kolano1, 27,0xc04000); // 27
+//  P.elipsa3(150-2,365-3,29,90,0,180,170,0x602000);	  
+  // udo1
+  wypelnionaElipsa(udo1, 30-3,92+3,0,180,169,0xa02000);	  
   //wypelnionaElipsa(Xkolano1-41,Ykolano1+52,28,70,0,360,55,0xd06000);
   //P.elipsa3(Xkolano1-41+2,Ykolano1+52-1,27,73,0,360,55,0xd06000);
-  wypelnionaElipsa(Xkolano1-41+2,Ykolano1+52-1,27,73+4,0,360,55,0xd06000);
+  // lydka1
+  wypelnionaElipsa(lydka1 ,27,73+4,0,360,55,0xd06000);
 
-  // stopa2
-  P.elipsa3(259,455,29,42,0,360,0,0xff00);
-  wypelnionaElipsa(259,455,29,42,0,360,0,0xc00060);
+  punkt& kolano2 = punkty[3];
+  kolano2 = punkt(258,445);
+  punkt& lydka2 = punkty[4];
+  lydka2 = kolano2 + punkt(1,10);
+
+  // lydka2
+  P.elipsa3(lydka2.x,lydka2.y, 29,42,0,360,0,0xff00);
+  wypelnionaElipsa(lydka2, 29,42,0,360,0,0xc00060);
   // kolano2
-  P.elipsa3(258,445,30,99,0,180,171,0xff00);
-  wypelnionaElipsa(258,445,30,99,0,180,171,0xa00030);
+  P.elipsa3(kolano2.x,kolano2.y ,30,99,0,180,171,0xff00);
+  wypelnionaElipsa(kolano2, 30,99,0,180,171,0xa00030);
   
+  punkt& udo2A = punkty[5];
+  punkt& udo2B = punkty[6];
+  udo2A = punkt(202,302);
+  udo2B = punkt(234,344);
+
   // udo2
   P.pisak(50);
-  P.linia(202,302,234,344,0x4010);
+  linia(udo2A,udo2B, 0x4010);
   P.pisak(46);
-  P.linia(202,302,234,344,0xd00070);
+  linia(udo2A,udo2B, 0xd00070);
   
+  punkt& torsD = punkty[7];
+  punkt& torsC = punkty[8];
+  punkt& torsB = punkty[9];
+  punkt& torsA = punkty[10];  
+  torsD = punkt(155,257);
+  torsC = punkt(193,257);
+  torsB = punkt(195,129);
+  torsA = punkt(145,134);
+
   // tors
   P.pisak(56);
   // D,C
-  linia(155,257,193,257,0xa04080);
-  //liniaDo(193,257);
-  // B
-  liniaDo(195,129);
-  // A
-  liniaDo(145,134);
-  // D
-  liniaDo(155,257);
+  linia(torsD,torsC,0xa04080);
+  liniaDo(torsB);
+  liniaDo(torsA);
+  liniaDo(torsD);
 
+  // wzor na torsie
+
+  /* */
+  punkt& wzorA = punkty[11];
+  punkt& wzorC = punkty[12];
+  wzorA = torsA + punkt(5,5);
+  wzorC = torsC + punkt(2,2);
   // D,B:
   P.pisak(46);
-  linia(155,257,195,129,0); //0x302010);
+  linia(torsD,torsB,0); //0x302010);
   // A+(5,5):
-  //P.rysujPunkt4(145,134,0x0,56>>1);
-  wypelnionaElipsa(150,139,(56+6)>>1,(56+6)>>1,0,180,110,0); // 0x302010);
+  //P.wypelnionyOkrag(145,134,0x0,56>>1);
+  wypelnionaElipsa(wzorA,(56+6)>>1,(56+6)>>1,0,180,110,0); // 0x302010);
   // C+(2,2):
-  wypelnionaElipsa(195,259,(56-4)>>1,(56-4)>>1,0,180,290,0); // 0x302010);
-  
+  wypelnionaElipsa(wzorC,(56-4)>>1,(56-4)>>1,0,180,290,0); // 0x302010);
+  /* */
   //wypelnionaElipsa(145,134,17,26,0,360,30,0); // 0x302010);
 
+  punkt& glowa = punkty[13];
+  glowa = punkt(171,66);
   // glowa
-  P.rysujPunkt4(171,66,0x802040,37);
+  wypelnionyOkrag(glowa, 37,0x802040);
 
+  punkt& reka1 = punkty[14];
+  reka1 = punkt(65,177);
+  punkt& ramie1 = punkty[15];
+  ramie1 = reka1 + punkt(8+3, -8);
+
+#define KOLOROWY (0)
   // reka1
-  P.rysujPunkt4(65,177,0xff00,30);
-  wypelnionaElipsa(65,177,28,65,0,180,31.5,0xa4080);
-  wypelnionaElipsa(65+8+3,177-8,25,60,0,180,180+31.5+10,0xa4080);
+  wypelnionyOkrag(reka1, 30,0xff00);
+  wypelnionaElipsa(reka1, 28,65,0,180,31.5,KOLOROWY ? 0xa4080 : 0xa00050);
+  wypelnionaElipsa(ramie1 , 25,60,0,180,180+31.5+10,KOLOROWY ? 0xff0000 : 0xd00070); //0xa4080);
+
+  punkt& reka2 = punkty[16];
+  reka2 = punkt(300,200) - punkt(18, 15);
+  punkt& strzalkowa2 = punkty[17];
+  strzalkowa2 = reka2 + punkt(8+3, 8);
+  punkt& ramie2 = punkty[18];
+  ramie2 = reka2 + punkt(-5-5,-5);
 
   // reka2
-  punkt reka2(300, 200);
-  reka2 = reka2 - punkt(18, 15);
-  punkt rk2=reka2;
-  punkt rk2piszczel = reka2 + punkt(8+3, 8);
-  P.rysujPunkt4(reka2.x,reka2.y,0xff00,28);
-  wypelnionaElipsa(rk2.x-5-5, rk2.y-5, 27,80,0,180,41.5+90+15,0xa30a0);
-  wypelnionaElipsa(rk2piszczel.x, rk2piszczel.y, 26,65,0,180,180+41.5+10+90,0xa30a0);
+  wypelnionyOkrag(reka2, 28,0xff00);
+  wypelnionaElipsa(ramie2, 27,80,0,180,41.5+90+15,KOLOROWY ? 0xff0000 :  0xa06030); //0xa30a0);
+  wypelnionaElipsa(strzalkowa2, 26,65,0,180,180+41.5+10+90,KOLOROWY ? 0xa30a0 : 0xd08040 );
 
   
   P.odswiez();
